7a0d50c9816246f3a77f76ab1772f24f919e4139
mutembeijoe <mutembeijoe@gmail.com>
2019-11-08 16:56:42 +0300

    adds Location and Category Models, modifies the Image model to include location and category foreign Keys

--- gallery/models.py
+++ gallery/models.py
@@ -2,10 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=30)
+
+
 class Image(models.Model):
     image = models.ImageField(upload_to='profile_pics')
     name = models.CharField(max_length=30)
     description = models.TextField()
+    location = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name}"
+
