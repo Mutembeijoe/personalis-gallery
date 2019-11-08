74f24066a3dc4f8ff27e9181a89b2957f33084e1
mutembeijoe <mutembeijoe@gmail.com>
2019-11-08 17:29:37 +0300

    removes sensitive info from settings.py ready for first push to github

--- personalis_gallery/settings.py
+++ personalis_gallery/settings.py
@@ -77,11 +77,7 @@ WSGI_APPLICATION = 'personalis_gallery.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'gallery',
-        # 'USER': 'orion',
-        # 'PASSWORD': 'root'
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
 }
 
