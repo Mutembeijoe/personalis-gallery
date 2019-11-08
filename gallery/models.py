f89572481ebe56dbfe285c531847ef28281162da
mutembeijoe <mutembeijoe@gmail.com>
2019-11-08 16:32:13 +0300

    adds an initial Image model

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='profile_pics')
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.name}"
--- personalis_gallery/settings.py
+++ personalis_gallery/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'gallery.apps.GalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,6 +78,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'gallery',
+        # 'USER': 'orion',
+        # 'PASSWORD': 'root'
     }
 }
 
@@ -106,6 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
+# TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
