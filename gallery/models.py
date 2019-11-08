2c7d01941f274f9527688e8ae5fcbfe3a5b24edf
mutembeijoe <mutembeijoe@gmail.com>
2019-11-08 17:27:41 +0300

    adds __str__ magic methods to my model classes for their display

--- gallery/models.py
+++ gallery/models.py
@@ -6,9 +6,15 @@ from django.db import models
 class Location(models.Model):
     name = models.CharField(max_length=30)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Category(models.Model):
     title = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.title
 
 
 class Image(models.Model):
@@ -16,7 +22,7 @@ class Image(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
     location = models.ForeignKey(Location, null=True, on_delete=models.SET_NULL)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.name}"
